564115948ce0a555738b6328dbbd217e3c3850c3
bisu07 <g26.bishal@gmail.com>
2021-09-28 22:27:53 -0500

    this is first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace operatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 15;
+            int num2 = 6;
+            var addition = num1 + num2;
+            var subtraction = num1 - num2;
+            var quotient = num1 / num2;
+            var remainder = num1 % num2;
+            Console.WriteLine($"The result of {num1}/{num2} is {quotient} amd remainder is {remainder}");
+
+        }
+    }
+}
